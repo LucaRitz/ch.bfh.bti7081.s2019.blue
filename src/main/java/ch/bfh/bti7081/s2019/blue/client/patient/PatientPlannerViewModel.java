7b034540d5f9f1ab384338e69ccba76246bfa8b0
luca <ritz.luca@gmail.com>
2019-05-10 16:40:09 +0200

    Übersetzungen werden über Template-Modell gesetzt.

---
+++ src/main/java/ch/bfh/bti7081/s2019/blue/client/patient/PatientPlannerViewModel.java
@@ -0,0 +1,14 @@
+package ch.bfh.bti7081.s2019.blue.client.patient;
+
+import com.vaadin.flow.templatemodel.TemplateModel;
+
+public interface PatientPlannerViewModel extends TemplateModel {
+
+    Text getText();
+
+    interface Text {
+        void setTitle(String title);
+        void setPrevious(String previous);
+        void setNext(String next);
+    }
+}
