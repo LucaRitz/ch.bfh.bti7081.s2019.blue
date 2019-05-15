63f2c822eaff7ce88a7148cf6f0ea2aa93173747
Lukas Seglias <lukas.seglias@gmail.com>
2019-05-15 19:54:31 +0200

    Correct navigation item order

--- src/main/java/ch/bfh/bti7081/s2019/blue/client/home/HomeEntryPoint.java
+++ src/main/java/ch/bfh/bti7081/s2019/blue/client/home/HomeEntryPoint.java
@@ -1,6 +1,7 @@
 package ch.bfh.bti7081.s2019.blue.client.home;
 
 import ch.bfh.bti7081.s2019.blue.client.base.Layout;
+import ch.bfh.bti7081.s2019.blue.client.base.Navigation;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.PWA;
@@ -9,7 +10,7 @@ import com.vaadin.flow.spring.annotation.UIScope;
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 
-@Route(value = "home", layout = Layout.class)
+@Route(value = Navigation.HOME, layout = Layout.class)
 @PWA(name = "Project Base for Vaadin Flow", shortName = "Project Base")
 @UIScope
 public class HomeEntryPoint extends Div {
