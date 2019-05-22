856d05bb625e2d0577ee2c0ae5a0cb45a2b5b4c2
Lukas Seglias <lukas.seglias@gmail.com>
2019-05-22 19:39:17 +0200

    Added mission series employee recommendations backend

--- src/main/java/ch/bfh/bti7081/s2019/blue/server/service/EmployeeRecommendationService.java
+++ src/main/java/ch/bfh/bti7081/s2019/blue/server/service/EmployeeRecommendationService.java
@@ -4,13 +4,15 @@ import ch.bfh.bti7081.s2019.blue.server.persistence.EmployeeRepository;
 import ch.bfh.bti7081.s2019.blue.server.persistence.MissionCountPerEmployee;
 import ch.bfh.bti7081.s2019.blue.server.persistence.PatientRepository;
 import ch.bfh.bti7081.s2019.blue.server.persistence.model.Employee;
+import ch.bfh.bti7081.s2019.blue.server.persistence.model.MissionSeries;
+import ch.bfh.bti7081.s2019.blue.server.persistence.model.Patient;
 import ch.bfh.bti7081.s2019.blue.shared.dto.DateRange;
 import ch.bfh.bti7081.s2019.blue.shared.dto.MissionDto;
-import ch.bfh.bti7081.s2019.blue.shared.dto.PatientRefDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Comparator;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -51,10 +53,10 @@ public class EmployeeRecommendationService {
         return missions;
     }
 
-    public List<Employee> getEmployeeRecommendations(MissionDto mission) {
+    public List<Employee> getEmployeeRecommendations(MissionSeries missionSeries, Date startDate, Date endDate) {
 
-        DateRange dateRange = new DateRange(mission.getStartDate(), mission.getEndDate());
-        PatientRefDto patient = mission.getMissionSeries().getPatient();
+        DateRange dateRange = new DateRange(startDate, endDate);
+        Patient patient = missionSeries.getPatient();
 
         Schedule schedule = getSchedule(dateRange);
         PatientFamiliarityScores scores = getFamiliarityScore(patient);
@@ -65,7 +67,7 @@ public class EmployeeRecommendationService {
                 .collect(Collectors.toList());
     }
 
-    private PatientFamiliarityScores getFamiliarityScore(PatientRefDto patient) {
+    private PatientFamiliarityScores getFamiliarityScore(Patient patient) {
 
         PatientFamiliarityScores patientFamiliarityScores = new PatientFamiliarityScores();
 
