caaaef69e13651780a9059b49d4c9e9d5a671504
Christian Nussbaum <36642170+Christian-Nussbaum@users.noreply.github.com>
2019-05-29 18:48:07 +0200

    Employee daily overview (#66)
    * Added Employee Daily Overview
    
    * Added data bindings
    
    * Fixed some minor things
    
    * Fixed issues from pull request

---
+++ src/main/java/ch/bfh/bti7081/s2019/blue/client/dailyoverview/EmployeeDailyOverviewEntryPoint.java
@@ -0,0 +1,24 @@
+package ch.bfh.bti7081.s2019.blue.client.dailyoverview;
+
+import ch.bfh.bti7081.s2019.blue.client.app.base.Layout;
+import ch.bfh.bti7081.s2019.blue.client.app.base.Navigation;
+import com.vaadin.flow.component.html.Div;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.spring.annotation.UIScope;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.PostConstruct;
+
+@Route(value = Navigation.EMPLOYEE_DAILY_OVERVIEW, layout = Layout.class)
+@UIScope
+public class EmployeeDailyOverviewEntryPoint extends Div {
+
+    @Autowired
+    private EmployeeDailyOverviewActivity presenter;
+
+    @PostConstruct
+    void setUp() {
+        add(presenter.getView().asComponent());
+        presenter.start();
+    }
+}
